134e85c308eaba94ad84ce48efa9ce21db7b2448
BuiXuanSon1412 <bxson1412@gmail.com>
2025-09-05 22:05:46 +0700

    add: repairing procedure for capacity

--- src/encoder.py
+++ src/encoder.py
@@ -1,5 +1,14 @@
-from math import dist
-from population import Problem, Individual, Solution
+from copy import deepcopy
+import re
+from sys import dont_write_bytecode
+from population import Individual
+from problem import Problem
+
+
+class Solution:
+    truck_routes: list[list[tuple[int, int]]]
+    drone_routes: list[list[list[tuple[int, int]]]]
+
 
 dist_mtx_truck: list[list[float]] = [[]]
 dist3: list[list[list[float]]] = [[[]]]
@@ -21,17 +30,16 @@ def fair_split(chro, problem: Problem):
     avg_dist = sum(dist_mtx_truck[0]) / len(dist_mtx_truck[0])
 
     # estimate total distance of all truck routes
-    sum_dist = (problem.num_truck - 1) * avg_dist
+    sum_dist = (problem.K - 1) * avg_dist
     sum_dist += dist_mtx_truck[0][len(chro) - 1] + dist_mtx_truck[len(chro) - 1][0]
 
     for i in range(1, len(chro)):
         sum_dist += dist_mtx_truck[i - 1][i]
 
     # estimate average total distance of truck routes
-    dist_per_route = sum_dist / problem.num_truck
+    dist_per_route = sum_dist / problem.K
 
     routes = []
-    dists = []
     i = 0
     while i < len(chro):
         route = []
@@ -44,7 +52,7 @@ def fair_split(chro, problem: Problem):
             + dist_mtx_truck[chro[i]][0]
             > dist_per_route
         ):
-            route.append((chro[i], problem.upper_volume[chro[i]]))
+            route.append((chro[i], problem.customer_list[chro[i]].u))
             tmp_dist = tmp_dist + dist_mtx_truck[tmp_cust][chro[i]]
             tmp_cust = chro[i]
             i = i + 1
@@ -53,13 +61,11 @@ def fair_split(chro, problem: Problem):
         ] + dist_mtx_truck[chro[i]][0] - dist_per_route < dist_per_route - (
             tmp_dist + dist_mtx_truck[tmp_cust][0]
         ):
-            route.append((chro[i], problem.upper_volume[chro[i]]))
+            route.append((chro[i], problem.customer_list[chro[i]].u))
             tmp_dist = tmp_dist + dist_mtx_truck[tmp_cust][chro[i]]
             tmp_cust = chro[i]
             i = i + 1
-        tmp_dist = tmp_dist + dist_mtx_truck[tmp_cust][0]
         routes.append(route)
-        dists.append(tmp_dist)
     return routes
 
 
@@ -129,55 +135,127 @@ def relax_dist(parts, problem: Problem):
     # change position
 
 
-def customer_in_range(range, problem: Problem):
-    customers = []
-    for customer in range(1, problem.num_customer):
-        if 2 * dist_mtx_truck[0][customer] <= range:
-            customers.append(customer)
-    return customers
+def repair_cap_by_truck(vio_route, routes, problem):
+    # find postion for each node where it can be insered into
+    for idx in range(len(vio_route)):
+        if problem.check_truck_capacity(vio_route):
+            return
 
+        cust = vio_route[idx][0]
+        vol = vio_route[idx][1]
 
-def relax_cap(violated_route, not_violated, problem: Problem):
-    rem = sum(i[1] for i in violated_route[1]) - problem.truck_capacity
+        for i in range(len(routes)):
+            route = routes[i]
+            left_cap = problem.cal_route_volume(route) - problem.Q
 
-    # sorted_node = sorted(violated_route[1], key=lambda x: problem.unit_profit[x[0]])
-
-    # take from customer with the lowest profit unit
-    for node in violated_route[1]:
-        # search in all non-violated routes to find the insertion index which does not violate time limit
-        # also be the maximum inserted volume
-
-        for route in not_violated:
-            distance = route[0]
-            path = route[1].copy()
-            rem_cap = problem.truck_capacity - sum(i[1] for i in path)
-
-            if not rem_cap:
-                continue
-
-            path.insert((0, 0))
-            path.append((0, 0))
-
-            id = 0
-            taken = 0
-
-            if (
-                distance + dist3[0][node[0]][path[0]] - dist_mtx_truck[0][path[0]]
-                <= problem.system_duration * problem.drone_speed
-            ):
-                tmp = min(node[1], rem_cap)
-                if taken < tmp:
-                    taken = tmp
-                    id = 0
-            for k in range(len(path)):
+            # find in each route, where should be inserted
+            taken = None
+            for j in range(len(route) + 1):
+                tmp_cust = route[j][0] if j < len(route) else 0
+                tmp_prev_cust = route[j - 1][0] if j > 0 else 0
                 if (
-                    distance + dist3[0][node[0]][path[0]] - dist_mtx_truck[0][path[0]]
-                    <= problem.system_duration * problem.drone_speed
+                    problem.cal_route_truck_time(vio_route)
+                    - dist_mtx_truck[tmp_prev_cust][tmp_cust]
+                    + dist_mtx_truck[tmp_prev_cust][cust]
+                    + dist_mtx_truck[cust][tmp_cust]
+                    <= problem.v * problem.T
                 ):
-                    tmp = min(node[1], rem_cap)
-                    if taken <= tmp:
-                        taken = tmp
-                        id = k
+                    if vol > left_cap:
+                        taken = left_cap
+                        vol -= taken
+                        routes[i].insert(j, (cust, taken))
+                        vio_route[idx][1] = vol
+                    else:
+                        routes[i].insert(j, (cust, vol))
+                        vio_route.pop(idx)
+
+
+def repair_cap_by_drone(
+    vio_route, drone_routes, routes, problem: Problem
+) -> list[list[tuple[int, int]]]:
+    rem_vol = sum(node[1] for node in vio_route) - problem.Q
+
+    reachable = []
+
+    # check if there is any reachable node in route
+    for idx in range(len(vio_route)):
+        node = vio_route[idx]
+        if problem.check_drone_energy_constraint([node]):
+            reachable.append((idx, node))
+
+    if not reachable:
+        return drone_routes
+
+    while not problem.check_drone_time_constraint(drone_routes):
+        # find route for each drone
+        for i in range(problem.D):
+            route = []
+            tmp_cap = problem.tilde_Q
+            tmp_cust = 0
+
+            while True:
+                nearest = None
+                dist = 0
+                for idx in range(len(vio_route)):
+                    if tmp_cust == vio_route[idx][0]:
+                        continue
+                    # check validity of selection with drone constraints
+                    tmp_route = [route[:], vio_route[idx]]
+                    tmp_drone_routes = deepcopy(drone_routes)
+                    tmp_drone_routes[i].append(tmp_route)
+
+                    if not problem.check_drone_time_constraint(
+                        tmp_drone_routes
+                    ) or not problem.check_drone_energy_constraint(tmp_route):
+                        continue
+
+                    if not nearest:
+                        nearest = idx
+                        dist = dist_mtx_drone[tmp_cust][vio_route[idx][0]]
+                    elif dist > dist_mtx_drone[tmp_cust][vio_route[idx][0]]:
+                        nearest = idx
+                        dist = dist_mtx_drone[tmp_cust][vio_route[idx][0]]
+
+                if not nearest:
+                    break
+
+                tmp_vol = min(tmp_cap, vio_route[nearest][1])
+                tmp_cap -= tmp_vol
+                vio_route[nearest][1] -= tmp_vol
+                rem_vol -= tmp_vol
+
+                if vio_route[nearest][1] == 0:
+                    vio_route.pop(nearest)
+
+                route.append((vio_route[nearest][0], tmp_vol))
+
+                if rem_vol <= 0:
+                    break
+
+            drone_routes[i].append(route)
+            if rem_vol <= 0:
+                return drone_routes
+    return drone_routes
+
+
+def repair_cap_by_lowering(problem: Problem):
+    pass
+
+
+def relax_cap(vio_cap_routes, drone_routes, routes, problem: Problem):
+    vio = False
+
+    for i in range(len(vio_cap_routes)):
+        # resolve each route iteratively
+        repair_cap_by_truck(vio_cap_routes[i], routes, problem)
+
+        if problem.check_truck_capacity(vio_cap_routes[i]):
+            repair_cap_by_drone(vio_cap_routes[i], drone_routes, routes, problem)
+
+        if problem.check_truck_capacity(vio_cap_routes[i]):
+            pass
+
+    repair_cap_by_lowering(problem)
 
 
 def decode2(indi: Individual, problem: Problem):
@@ -188,49 +266,31 @@ def decode2(indi: Individual, problem: Problem):
 
     sol = Solution()
 
-    routes, dists = fair_split(chro, problem)
+    routes = fair_split(chro, problem)
 
-    max_truck_dist = problem.system_duration * problem.truck_speed
-    max_drone_dist = problem.drone_duration * problem.drone_speed
+    vio_dist_routes = []
+    vio_cap_routes = []
 
-    if all(route[0] <= max_truck_dist for route in routes):
-        if all(
-            (sum(i[1] for i in route[1]) <= problem.truck_capacity) for route in routes
-        ):
-            # no capacity and duration violation
-            sol.truck_routes = [route[1] for route in routes]
+    for route in routes:
+        if problem.check_truck_time_constraint(route):
+            vio_dist_routes.append(route)
+        if problem.check_truck_capacity(route):
+            vio_cap_routes.append(routes.pop(route))
+
+    routes = [
+        route
+        for route in routes
+        if route not in vio_cap_routes and route not in vio_dist_routes
+    ]
+
+    if not vio_dist_routes:
+        if not vio_cap_routes:
+            sol.truck_routes = routes
             sol.drone_routes = []
             return sol
         else:
-            # capacity violation
-            not_violated = []
-            violated = []
-
-            for route in routes:
-                volume = sum(i[1] for i in route[1])
-                if volume <= problem.truck_capacity:
-                    not_violated.append(route)
-                else:
-                    violated.append(route)
-
-            # split volume for non-violated truck
-            while violated:
-                for route in violated:
-                    # take the node with lowest
-                    x = relax_cap(route, not_violated, problem)
-                    if x:
-                        violated.remove(route)
-
-            # if drones possibly solve reduntant volume
+            drone_routes = []
+            relax_cap(vio_cap_routes, drone_routes, routes, problem)
 
     else:
-        # relax_dist(parts, problem)
-        # split freight to drones to satisfy system time limit
-
-        # split freight for drone
-        if all(
-            (sum(i[1] for i in route[1]) <= problem.truck_capacity) for route in routes
-        ):
-            pass
-        else:
-            pass
+        pass
--- src/problem.py
+++ src/problem.py
@@ -6,12 +6,27 @@ class Customer:
         self.t = service_time
         self.x = x
         self.y = y
-        
+
+
 class Problem:
-    def __init__(self, customer_list, number_of_trucks, number_of_drones, distance_matrix_truck,
-                distance_matrix_drone, truck_capacity, drone_capacity, drone_energy,
-                speed_of_truck, speed_of_drone, launch_time, land_time, system_time,
-                energy_consumption_rate, weight_of_drone):
+    def __init__(
+        self,
+        customer_list,
+        number_of_trucks,
+        number_of_drones,
+        distance_matrix_truck,
+        distance_matrix_drone,
+        truck_capacity,
+        drone_capacity,
+        drone_energy,
+        speed_of_truck,
+        speed_of_drone,
+        launch_time,
+        land_time,
+        system_time,
+        energy_consumption_rate,
+        weight_of_drone,
+    ):
         self.customer_list = customer_list
         self.K = number_of_trucks
         self.D = number_of_drones
@@ -46,17 +61,24 @@ class Problem:
         else:
             return False
 
-        
     def cal_truck_route_time(self, route):
         total_time = 0
         if len(route) == 0:
             return 0
-        total_time = total_time + self.d[0][route[0][0]]/self.v + self.customer_list[route[0][0]].t
+        total_time = (
+            total_time
+            + self.d[0][route[0][0]] / self.v
+            + self.customer_list[route[0][0]].t
+        )
         for i in range(len(route) - 1):
-            total_time = total_time + self.d[route[i][0]][route[i+1][0]]/self.v + self.customer_list[route[i+1][0]].t
-        total_time = total_time + self.d[route[-1][0]][0]/self.v
+            total_time = (
+                total_time
+                + self.d[route[i][0]][route[i + 1][0]] / self.v
+                + self.customer_list[route[i + 1][0]].t
+            )
+        total_time = total_time + self.d[route[-1][0]][0] / self.v
         return total_time
-    
+
     def check_truck_time_constraint(self, route):
         total_time = self.cal_truck_route_time(route)
         if total_time <= self.T:
@@ -68,12 +90,29 @@ class Problem:
         total_time = 0
         if len(route) == 0:
             return 0
-        total_time = total_time + self.tau_l + self.tilde_d[0][route[0][0]]/self.tilde_v + self.tau_r + self.customer_list[route[0][0]].t
+        total_time = (
+            total_time
+            + self.tau_l
+            + self.tilde_d[0][route[0][0]] / self.tilde_v
+            + self.tau_r
+            + self.customer_list[route[0][0]].t
+        )
         for i in range(len(route) - 1):
-            total_time = total_time + self.tau_l + self.tilde_d[route[i][0]][route[i+1][0]]/self.tilde_v + self.tau_r + self.customer_list[route[i+1][0]].t
-        total_time = total_time + self.tau_l + self.tilde_d[route[-1][0]][0]/self.tilde_v + self.tau_r
+            total_time = (
+                total_time
+                + self.tau_l
+                + self.tilde_d[route[i][0]][route[i + 1][0]] / self.tilde_v
+                + self.tau_r
+                + self.customer_list[route[i + 1][0]].t
+            )
+        total_time = (
+            total_time
+            + self.tau_l
+            + self.tilde_d[route[-1][0]][0] / self.tilde_v
+            + self.tau_r
+        )
         return total_time
-    
+
     def check_drone_time_constraint(self, multi_route):
         total_time = 0
         for route in multi_route:
@@ -91,22 +130,49 @@ class Problem:
         for customer in route:
             total_demand = total_demand + customer[1]
 
-        total_energy = total_energy + self.lbda*(self.tilde_qo + total_demand)*self.tilde_d[0][route[0][0]]/self.tilde_v 
+        total_energy = (
+            total_energy
+            + self.lbda
+            * (self.tilde_qo + total_demand)
+            * self.tilde_d[0][route[0][0]]
+            / self.tilde_v
+        )
         total_demand = total_demand - route[0][1]
-        total_energy = total_energy + self.lbda* total_demand * self.customer_list[route[0][0]].t
+        total_energy = (
+            total_energy + self.lbda * total_demand * self.customer_list[route[0][0]].t
+        )
         for i in range(len(route) - 1):
-            total_energy = total_energy + self.lbda*(self.tilde_qo + total_demand)*self.tilde_d[route[i][0]][route[i+1][0]]/self.tilde_v
-            total_demand = total_demand - route[i+1][1]
-            total_energy = total_energy + self.lbda* total_demand * self.customer_list[route[i+1][0]].t
+            total_energy = (
+                total_energy
+                + self.lbda
+                * (self.tilde_qo + total_demand)
+                * self.tilde_d[route[i][0]][route[i + 1][0]]
+                / self.tilde_v
+            )
+            total_demand = total_demand - route[i + 1][1]
+            total_energy = (
+                total_energy
+                + self.lbda * total_demand * self.customer_list[route[i + 1][0]].t
+            )
 
-        total_energy = total_energy + self.lbda*(self.tilde_qo + total_demand)*self.tilde_d[route[-1][0]][0]/self.tilde_v 
+        total_energy = (
+            total_energy
+            + self.lbda
+            * (self.tilde_qo + total_demand)
+            * self.tilde_d[route[-1][0]][0]
+            / self.tilde_v
+        )
         return total_energy
-    
+
     def check_drone_energy_constraint(self, route):
         total_energy = self.cal_drone_route_energy(route)
         if total_energy <= self.E:
             return True
         else:
             return False
-        
 
+    def cal_route_volume(self, route):
+        vol = 0
+        for i in range(len(route)):
+            vol += route[i][1]
+        return vol
